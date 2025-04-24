fcfccbf97f05ec222ad2b1974b4c61f77e4fab22
Hariprasad G <101337307+HariprasadG92@users.noreply.github.com>
2025-04-24 17:41:16 +0530

    Delete task3 directory

--- task3/BorgConnect_Chart/borg_front/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('welcome/', views.welcome_view, name='welcome'),  # 👈 This renders dashboard.html
-    path('api/chart-data/', views.get_chart_data, name='chart_data'),
-]
